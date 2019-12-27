a3085fac932f1b17c5bc70462a20da4914fc43f2
chicu <chc1129@gmail.com>
2019-12-27 19:24:42 +0900

    fix commit

---
+++ Unit-0/lesson01/playground/playground.go
@@ -0,0 +1,9 @@
+package main // このコードが属するパッケージを宣言する
+
+import (
+  "fmt"  // fmt(format)パッケージを使えるようにする
+)
+
+func main() { // mainという名前の関数を宣言する
+  fmt.Println("Hello, playground")  // Hello, playgroundと画面に表示する
+}
